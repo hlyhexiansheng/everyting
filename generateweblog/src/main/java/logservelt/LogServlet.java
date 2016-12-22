1e7de03e1438fe21e4c014e05181f8c9b40df8a0
noodles.he <noodles.he@uxin.com>
2016-12-22 14:31:19 +0800

    .....................

--- generateweblog/src/main/java/logservelt/LogServlet.java
+++ generateweblog/src/main/java/logservelt/LogServlet.java
@@ -22,23 +22,16 @@ public class LogServlet extends HttpServlet{
     public static final Logger logger = LogManager.getLogger(LogServlet.class);
 ////public static final Logger logger = Logger.getLogger(LogServlet.class);
 //    public static final Logger logger = LoggerFactory.getLogger(LogServlet.class);
+    private static long logId = 0;
     @Override
     public void init() throws ServletException {
 
-//        System.out.println(logger.getName());
-//        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-//        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
-//            public void run() {
-//                logger.warn("warn log");
-//                System.out.println("----------------");
-//                logger.warn("warn log,{},{}",1,"1212");
-//                System.out.println("----------------");
-//                logger.info("Info log",new RuntimeException("Exception"));
-//                System.out.println("----------------");
-//                logger.error("error log");
-//                System.out.println("----------------");
-//            }
-//        },1,3, TimeUnit.SECONDS);
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                logger.error("error log [{}]" ,logId++);
+            }
+        },1,10, TimeUnit.MILLISECONDS);
 
         super.init();
     }
--- test.web/src/main/java/lambda/Test.java
+++ test.web/src/main/java/lambda/Test.java
@@ -1,10 +1,13 @@
 package lambda;
 
 import com.alibaba.fastjson.JSON;
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by noodles on 16/4/10.
@@ -13,10 +16,10 @@ public class Test {
 
     public static void main(String[] args) throws InterruptedException {
 
-        HashMap<CharSequence,CharSequence> map = new HashMap();
-        map.put("key1","val1");
-
-        final CharSequence sequence = map.get("key1");
-
+        Pattern p = Pattern.compile("^[0-9a-zA-Z_-]{1,}$");// 正则表达式匹配手机号
+        Matcher m = p.matcher("_-11232fdsA");
+        final boolean matches = m.matches();
+        System.out.println(matches);
     }
+
 }
